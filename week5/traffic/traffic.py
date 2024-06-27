c8072bd634cc936c0d891176f7ee69739a2857f0
Napoage <nicholas.poage@yahoo.com>
2024-06-27 18:04:29 -0500

    finished traffic.py

--- week5/traffic/traffic.py
+++ week5/traffic/traffic.py
@@ -57,8 +57,24 @@ def load_data(data_dir):
     numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
+
+    The load_data function should accept as an argument data_dir, representing the path to a directory where the data is stored, and return image arrays and labels for each image in the data set.
+    You may assume that data_dir will contain one directory named after each category, numbered 0 through NUM_CATEGORIES - 1. Inside each category directory will be some number of image files.
+    Use the OpenCV-Python module (cv2) to read each image as a numpy.ndarray (a numpy multidimensional array). To pass these images into a neural network, the images will need to be the same size, so be sure to resize each image to have width IMG_WIDTH and height IMG_HEIGHT.
+    The function should return a tuple (images, labels). images should be a list of all of the images in the data set, where each image is represented as a numpy.ndarray of the appropriate size. labels should be a list of integers, representing the category number for each of the corresponding images in the images list.
+    Your function should be platform-independent: that is to say, it should work regardless of operating system. Note that on macOS, the / character is used to separate path components, while the \ character is used on Windows. Use os.sep and os.path.join as needed instead of using your platform’s specific separator character.
     """
-    raise NotImplementedError
+    print(data_dir)
+    labels = []
+    images = []
+    for i in range(NUM_CATEGORIES):
+        path = os.path.join(data_dir, str(i))
+        for file in os.listdir(path):
+            image = cv2.imread(os.path.join(path, file))
+            image = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT))
+            images.append(image)
+            labels.append(i)
+    return images, labels
 
 
 def get_model():
@@ -66,8 +82,31 @@ def get_model():
     Returns a compiled convolutional neural network model. Assume that the
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
+
+    The get_model function should return a compiled neural network model.
+    You may assume that the input to the neural network will be of the shape (IMG_WIDTH, IMG_HEIGHT, 3) (that is, an array representing an image of width IMG_WIDTH, height IMG_HEIGHT, and 3 values for each pixel for red, green, and blue).
+    The output layer of the neural network should have NUM_CATEGORIES units, one for each of the traffic sign categories.
+    The number of layers and the types of layers you include in between are up to you. You may wish to experiment with:
+    different numbers of convolutional and pooling layers
+    different numbers and sizes of filters for convolutional layers
+    different pool sizes for pooling layers
+    different numbers and sizes of hidden layers
+    dropout
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(128, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+    
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
 
 
 if __name__ == "__main__":
